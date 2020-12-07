6d6dd3fa16211a2d90244d565d00a4f6253a4cfa
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-12-07 17:41:27 +0200

    add folder ContactList

--- src/App.js
+++ src/App.js
@@ -2,28 +2,41 @@
 import React, { Component } from "react";
 import { Fragment } from "react";
 import shortid from "shortid";
-import ContactList from "./components/ContactList";
+import ContactList from "./components/ContactList/ContactList";
 
 export default class App extends Component {
   state = {
     contacts: [
-      {
-        id: shortid.generate(),
-        name: "Anna",
-        number: "333",
-      },
-      {
-        id: shortid.generate(),
-        name: "Serhiy",
-        number: "555",
-      },
-      {
-        id: shortid.generate(),
-        name: "Tanja",
-        number: "999",
-      },
+      // {
+      //   id: shortid.generate(),
+      //   name: "Anna",
+      //   number: "333",
+      // },
+      // {
+      //   id: shortid.generate(),
+      //   name: "Serhiy",
+      //   number: "555",
+      // },
+      // {
+      //   id: shortid.generate(),
+      //   name: "Tanja",
+      //   number: "999",
+      // },
     ],
     name: "",
+    number: "",
+  };
+
+  onInputChange = (e) => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
   };
 
   deleteContact = (contactId) => {
@@ -34,19 +47,34 @@ export default class App extends Component {
     }));
   };
 
-  // addContact = (data) => {
-  //   return {
-  //     id: "aaa",
-  //     name: "Anna",
-  //     number: "333",
-  //   };
-  // };
-
   render() {
     const { contacts } = this.state;
+    const inputNameId = shortid.generate();
+    const inputNumberId = shortid.generate();
     return (
       <Fragment>
         <h1>Phonebook</h1>
+        <form onSubmit={this.onFormSubmit}>
+          <label htmlFor={inputNameId}>Name</label>
+          <input
+            type="text"
+            name="name"
+            id={inputNameId}
+            value={this.state.name}
+            onChange={this.onInputChange}
+          />
+
+          <label htmlFor={inputNumberId}>Number</label>
+          <input
+            type="tel"
+            name="number"
+            id={inputNumberId}
+            value={this.state.number}
+            onChange={this.onInputChange}
+          />
+
+          <button type="submit">Add contact</button>
+        </form>
         <h2>Contacts</h2>
         <ContactList items={contacts} onDeleteContact={this.deleteContact} />
       </Fragment>
