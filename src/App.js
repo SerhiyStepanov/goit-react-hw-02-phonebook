95f4b0b10eccef0cb8cfac2ebcd4c56ab22d3440
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-12-07 20:48:06 +0200

    add data and update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-// import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { Fragment } from "react";
 import shortid from "shortid";
-import ContactList from "./components/ContactList/ContactList";
+import Form from "./components/Form";
+import ContactList from "./components/ContactList";
 
 export default class App extends Component {
   state = {
-    contacts: [
-      // {
-      //   id: shortid.generate(),
-      //   name: "Anna",
-      //   number: "333",
-      // },
-      // {
-      //   id: shortid.generate(),
-      //   name: "Serhiy",
-      //   number: "555",
-      // },
-      // {
-      //   id: shortid.generate(),
-      //   name: "Tanja",
-      //   number: "999",
-      // },
-    ],
-    name: "",
-    number: "",
+    contacts: [],
   };
 
-  onInputChange = (e) => {
-    const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
-  };
+  // onInputChange = (e) => {
+  //   const { name, value } = e.currentTarget;
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // };
 
-  onFormSubmit = (e) => {
-    e.preventDefault();
-    console.log(this.state);
+  // onFormSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(this.state);
+  // };
+
+  submitFormHandler = (data) => {
+    console.log(data);
+    const contact = {
+      id: shortid.generate(),
+      name: data.name,
+      number: data.number,
+    };
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, contact],
+    }));
   };
 
   deleteContact = (contactId) => {
@@ -49,32 +43,14 @@ export default class App extends Component {
 
   render() {
     const { contacts } = this.state;
-    const inputNameId = shortid.generate();
-    const inputNumberId = shortid.generate();
+    // const inputNameId = shortid.generate();
+    // const inputNumberId = shortid.generate();
     return (
       <Fragment>
         <h1>Phonebook</h1>
-        <form onSubmit={this.onFormSubmit}>
-          <label htmlFor={inputNameId}>Name</label>
-          <input
-            type="text"
-            name="name"
-            id={inputNameId}
-            value={this.state.name}
-            onChange={this.onInputChange}
-          />
 
-          <label htmlFor={inputNumberId}>Number</label>
-          <input
-            type="tel"
-            name="number"
-            id={inputNumberId}
-            value={this.state.number}
-            onChange={this.onInputChange}
-          />
+        <Form onSubmit={this.submitFormHandler} />
 
-          <button type="submit">Add contact</button>
-        </form>
         <h2>Contacts</h2>
         <ContactList items={contacts} onDeleteContact={this.deleteContact} />
       </Fragment>
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,2 +1,77 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import shortid from "shortid";
+import PropTypes from "prop-types";
 import s from "./Form.module.css";
+
+export default class Form extends Component {
+  state = {
+    name: "",
+    number: "",
+  };
+
+  onInputChange = (e) => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: "",
+      number: "",
+    });
+  };
+
+  render() {
+    const inputNameId = shortid.generate();
+    const inputNumberId = shortid.generate();
+
+    return (
+      <Fragment>
+        <form onSubmit={this.onFormSubmit}>
+          <div className={s.container}>
+            <label htmlFor={inputNameId} className={s.label}>
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id={inputNameId}
+              value={this.state.name}
+              onChange={this.onInputChange}
+            />
+          </div>
+
+          <div className={s.container}>
+            <label htmlFor={inputNumberId} className={s.label}>
+              Number
+            </label>
+            <input
+              type="tel"
+              name="number"
+              id={inputNumberId}
+              value={this.state.number}
+              onChange={this.onInputChange}
+            />
+          </div>
+
+          <button type="submit" className={s.button}>
+            Add contact
+          </button>
+        </form>
+      </Fragment>
+    );
+  }
+}
+
+Form.propTypes = {
+  onSubmit: PropTypes.func,
+};
